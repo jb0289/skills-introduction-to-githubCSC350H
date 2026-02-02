c27ccbc62b09bec6f7a5a4bb30115f5612de95ec
jb1645 <jb1645@j.berich87@gmail.com>
2026-02-02 11:34:50 -0500

    Completed Exercise Nested decision Logic

--- Program.cs
+++ Program.cs
@@ -1,26 +1,44 @@
 ﻿Random dice = new Random();
-int roll1 = dice.Next(1, 7); // Generates a random number between 1
-int roll2 = dice.Next(1, 7); // and 6 inclusive
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
 
+
+
 int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1}+ {roll2}+ {roll3} = {total}");
-if ((roll1 == roll2) && (roll2 == roll3))
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-}
-if ((roll1 == roll2) && (roll2 == roll3))
-{
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+    Console.WriteLine($"The total including the bonus: {total}");
 }
 
-if (total >= 15)
+
+if (total >= 16)
 {
-    Console.WriteLine("You win !");
+    Console.WriteLine("You win a new car!");
 }
- if (total < 15)
+else if (total >= 10)
 {
-    Console.WriteLine("Sorry you lose!");
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten.");
 }
--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa05d3fbaf9b12daed40bb1ba9749a607ea3227e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa71f29cbe6909b593379f3a3dd8250ea215e1e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
