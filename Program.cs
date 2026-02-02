7832fbd635c0cdc15a2ced0400a22804cd9d05a7
jb1645 <jb1645@j.berich87@gmail.com>
2026-02-02 09:45:37 -0500

    Completed Unit 3: Call methods from the .NET Class Library

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+﻿Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+int result = Random.Next();
