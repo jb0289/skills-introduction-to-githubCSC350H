a445bab626172afe140b5cd584beb6a4ecddebf6
jb1645 <jb1645@j.berich87@gmail.com>
2026-02-02 16:24:45 -0500

    Updated student grading report with separate exam/extra credit

--- Program.cs
+++ Program.cs
@@ -1,151 +1,146 @@
-<<<<<<< HEAD
-﻿/*
-   This code reverses a message, counts the number of times 
-   a certain character appears in the code, then prints the results.
+/* 
+This C# console application is designed to:
+- Use arrays to store student names and assignment scores.
+- Use a `foreach` statement to iterate through the student names as an outer program loop.
+- Use an `if` statement within the outer loop to identify the current student name and access that student's assignment scores.
+- Use a `foreach` statement within the outer loop to iterate though the assignment scores array and sum the values.
+- Use an algorithm within the outer loop to calculate the average exam score for each student.
+- Use an `if-elseif-else` construct within the outer loop to evaluate the average exam score and assign a letter grade automatically.
+- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
+    - detects extra credit assignments based on the number of elements in the student's scores array.
+    - divides the values of extra credit assignments by 10 before adding extra credit scores to the sum of exam scores.
+- use the following report format to report student grades: 
+
+Student         Exam Score      Overall Grade   Extra Credit
+
+Sophia          92.2            95.88   A       92 (3.68 pts)
+
 */
-
-string str = "The quick brown fox jumps over the lazy dog.";
-
-char[] charMessage = str.ToCharArray();
-Array.Reverse(charMessage);
-
-int oCount = 0;
-foreach (char character in charMessage)
-{
-    if (character == 'o')
-    {
-        oCount++;
-    }
-}
-
-string newMessage = new String(charMessage);
-
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {oCount} times.");
-=======
-﻿// initialize variables - graded assignments
 int examAssignments = 5;
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
-int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
-int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
-int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
 
-// Student names
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
-    
-    int[] studentScores = new int[10];
+int[] studentScores = new int[10];
 
 string currentStudentLetterGrade = "";
 
-// Write the Report Header to console
-Console.WriteLine("Student\t\tGrade\n");
+// display the header row for scores/grades
+Console.Clear();
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
 
+/*
+The outer foreach loop is used to:
+- iterate through student names 
+- assign a student's grades to the studentScores array
+- calculate exam and extra credit sums (inner foreach loop)
+- calculate numeric and letter grade
+- write the score report information
+*/
 foreach (string name in studentNames)
 {
     string currentStudent = name;
 
-if (currentStudent == "Sophia")
-       studentScores = sophiaScores;
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
 
-else if (currentStudent == "Andrew")
+    else if (currentStudent == "Andrew")
         studentScores = andrewScores;
 
-else if (currentStudent == "Emma")
+    else if (currentStudent == "Emma")
         studentScores = emmaScores;
 
-else if (currentStudent == "Logan")
+    else if (currentStudent == "Logan")
         studentScores = loganScores;
 
-else if (currentStudent == "Becky")
-        studentScores = beckyScores;
-
-else if (currentStudent == "Chris")
-    studentScores = chrisScores;
-
-else if (currentStudent == "Eric")
-    studentScores = ericScores;
-
-else if (currentStudent == "Gregor")
-    studentScores = gregorScores;
-else
-    continue;
-
-    // initialize/reset the sum assignments scores
-    int sumAssignmentScores = 0;
-
-    // initialize/reset the calculated average exam/extra credit grade
-    decimal currentStudentGrade = 0;
-
-    // initialize/reset a counter for the # of graded assignments
     int gradedAssignments = 0;
+    int gradedExtraCreditAssignments = 0;
 
-    // loop through the scores array 
+    int sumExamScores = 0;
+    int sumExtraCreditScores = 0;
+
+    decimal currentStudentGrade = 0;
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentExtraCreditScore = 0;
+
+    /* 
+    the inner foreach loop: 
+    - sums the exam and extra credit scores
+    - counts the extra credit assignments
+    */
     foreach (int score in studentScores)
     {
-        // increment the assignment counter
         gradedAssignments += 1;
 
-    if (gradedAssignments <= examAssignments)
-        // add the exam score to the sum
-        sumAssignmentScores += score;
+        if (gradedAssignments <= examAssignments)
+        {
+            sumExamScores = sumExamScores + score;
+        }
 
-    else
-        // add the extra credit points to the sum - bonus points earned
-        sumAssignmentScores += score / 10;
+        else
+        {
+            gradedExtraCreditAssignments += 1;
+            sumExtraCreditScores += score;
+        }
     }
 
-currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+    currentStudentExamScore = (decimal)(sumExamScores) / examAssignments;
+    currentStudentExtraCreditScore = (decimal)(sumExtraCreditScores) / gradedExtraCreditAssignments;
 
-if (currentStudentGrade >= 97)
-    currentStudentLetterGrade = "A+";
+    currentStudentGrade = (decimal)((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
 
-else if (currentStudentGrade >= 93)
-    currentStudentLetterGrade = "A";
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
 
-else if (currentStudentGrade >= 90)
-    currentStudentLetterGrade = "A-";
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
 
- else if (currentStudentGrade >= 87)
-     currentStudentLetterGrade = "B+";
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
 
-else if (currentStudentGrade >= 83)
-    currentStudentLetterGrade = "B";
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
 
-else if (currentStudentGrade >= 80)
-    currentStudentLetterGrade = "B-";
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
 
- else if (currentStudentGrade >= 77)
-    currentStudentLetterGrade = "C+";
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
 
-else if (currentStudentGrade >= 73)
-     currentStudentLetterGrade = "C";
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
 
-else if (currentStudentGrade >= 70)
-    currentStudentLetterGrade = "C-";
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
 
-else if (currentStudentGrade >= 67)
-    currentStudentLetterGrade = "D+";
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
 
-else if (currentStudentGrade >= 63)
-    currentStudentLetterGrade = "D";
-        
- else if (currentStudentGrade >= 60)
-    currentStudentLetterGrade = "D-";
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
 
     else
         currentStudentLetterGrade = "F";
 
-   
-    
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+
+    // Student         Exam Score      Overall Grade   Extra Credit
+    // Sophia          92.2            95.88   A       92 (3.68 pts)
+
+//new code 
+decimal extraCreditPoints = ((decimal)sumExtraCreditScores / 10) / examAssignments;
+Console.WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore} ({extraCreditPoints} pts)");
 }
 
-
+// required for running in VS Code (keeps the Output windows open to view results)
 Console.WriteLine("\n\rPress the Enter key to continue");
 Console.ReadLine();
->>>>>>> f3e525e (Completed Guided Project: CSC 350)
