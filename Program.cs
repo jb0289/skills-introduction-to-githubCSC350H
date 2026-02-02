2bab46cd9acdff972c5ba6db1d1804ee8d114e6c
jb1645 <jb1645@j.berich87@gmail.com>
2026-02-02 12:20:02 -0500

    Completed: Add decision logic

--- Program.cs
+++ Program.cs
@@ -1,44 +1,22 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+if (daysUntilExpiration == 0)
 {
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
-    Console.WriteLine($"The total including the bonus: {total}");
+    Console.WriteLine("Your subscription has expired.");
 }
-
-
-if (total >= 16)
+else if (daysUntilExpiration == 1)
 {
-    Console.WriteLine("You win a new car!");
+    Console.WriteLine("Your subscription expires within a day!");
+    Console.WriteLine("Renew now and save 20%!");
 }
-else if (total >= 10)
+else if (daysUntilExpiration <= 5)
 {
-    Console.WriteLine("You win a new laptop!");
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine("Renew now and save 10%!");
 }
-else if (total == 7)
+else if (daysUntilExpiration <= 10)
 {
-    Console.WriteLine("You win a trip for two!");
-}
-else
-{
-    Console.WriteLine("You win a kitten.");
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
