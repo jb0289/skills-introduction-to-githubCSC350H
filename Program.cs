0a7edcf23fc27915ce6fa7ca5bfcc5c1434d2f27
jb1645 <jb1645@j.berich87@gmail.com>
2026-02-02 12:57:16 -0500

    Completed Unit 7: Store and iterate Arrays

--- Program.cs
+++ Program.cs
@@ -1,22 +1,8 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-
-if (daysUntilExpiration == 0)
+﻿string[] Order_IDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string orderId in Order_IDs)
 {
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    Console.WriteLine("Renew now and save 20%!");
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    Console.WriteLine("Renew now and save 10%!");
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
+    if (orderId.StartsWith("B"))
+    {
+        Console.WriteLine(orderId);
+    }   
 }
--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa71f29cbe6909b593379f3a3dd8250ea215e1e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bab46cd9acdff972c5ba6db1d1804ee8d114e6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
