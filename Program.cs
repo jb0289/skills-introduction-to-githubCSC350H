aa71f29cbe6909b593379f3a3dd8250ea215e1e7
jb1645 <jb1645@j.berich87@gmail.com>
2026-02-02 11:08:10 -0500

    Completed Exercise - Create decision logic with if statements

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = System.Math.Max(firstValue, secondValue);
+﻿Random dice = new Random();
+int roll1 = dice.Next(1, 7); // Generates a random number between 1
+int roll2 = dice.Next(1, 7); // and 6 inclusive
+int roll3 = dice.Next(1, 7);
 
-Console.WriteLine(largerValue);
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1}+ {roll2}+ {roll3} = {total}");
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win !");
+}
+ if (total < 15)
+{
+    Console.WriteLine("Sorry you lose!");
+}
--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc8cf09a43e7c153947e09c207afd75cb93e7e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa05d3fbaf9b12daed40bb1ba9749a607ea3227e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
